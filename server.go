35d4a7c2ffac5409f2a37f48bcd7bf9bb37a01bd
punyapatsw <punyapat.sw@gmail.com>
2020-09-19 13:38:59 +0700

    add test for PairDeviceHandler

--- server.go
+++ server.go
@@ -11,12 +11,14 @@ import (
 	// _ "github.com/lib/pq"
 )
 
+func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status":"active"}`))
+}
+
 func main() {
 	fmt.Println("hello hometic")
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"status":"active"}`))
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
 	server := http.Server{
 		Addr:    "127.0.0.1:2009",
